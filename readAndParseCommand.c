ed5d7bbd4d9517da5a56312253f4fe7b8fd2d90e
minasmayth <minasmayth@gmail.com>
2022-02-22 16:22:34 +0100

    added framework to bridge our codes and starting with the fine parser now

--- readAndParseCommand.c
+++ readAndParseCommand.c
@@ -1,59 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
+struct commandList{
+    int numberOfCommands;
+    char** operators;
+    char* command[][3];
+};
 
 /*
-  List of builtin commands, followed by their corresponding functions.
+ * Fine parsing function to format commands
+ * into a style that the shell executor can
+ * understand.
  */
-char *builtin_str[] = {
-        "cd",
-        "help",
-        "exit"
-};
-int (*builtin_func[]) (char **) = {
-        &lsh_cd,
-        &lsh_help,
-        &lsh_exit
-};
+struct commandList fineParser(char **arguments) {
+    struct commandList result;
 
-#define BUFFER_SIZE 1024
+
+
+
+    return result;
+}
+
+void executeCommand(char* command[][3], int numberOfCommands, char** operators)
+{
+    int child,status, skip = 0, indexOfCommand = 0;
+
+    char **nextCommand;
+
+
+    while(indexOfCommand != numberOfCommands) {
+
+        //printf("skip?: %d\n", skip);
+        if(skip)
+        {
+            //take next command out of list
+            indexOfCommand++;
+            skip = 0;
+        }
+
+        nextCommand = command[indexOfCommand];
+
+        int savedStdOut = dup(1), savedStdIn = dup(0);
+
+        if (strchr(nextCommand[1], '<') != NULL)
+        {
+            printf("Found inputfile\n");
+            int fd0 = open("in", O_RDONLY);
+            dup2(fd0, STDIN_FILENO);
+            close(fd0);
+        }
+
+        if (strchr(nextCommand[1], '>') != NULL)
+        {
+            printf("Found outputfile\n");
+            int fd1 = creat("out" , 0644) ;
+            dup2(fd1, STDOUT_FILENO);
+            close(fd1);
+        }
+
+        //printf("nextCommand: %s\n", nextCommand[0]);
+
+        if((child=fork())==0) {
+            // execute the command entered by the user
+            execvp(nextCommand[0], nextCommand);
+            printf("Command %s not found!\n", nextCommand[0]);
+            exit(-1);
+        }
+        else
+        {
+            waitpid(-1,&status,0);
+
+            dup2(savedStdOut, 1);
+            close(savedStdOut);
+
+            dup2(savedStdIn, 0);
+            close(savedStdIn);
+
+
+            if(WEXITSTATUS(status) != 0)
+            {
+                printf("child exited with = %d\n",WEXITSTATUS(status));
+                if(operators[indexOfCommand] == "&&") skip = 1; //command failed and skip next
+            }
+        }
+        if (operators[indexOfCommand] == "||") skip = 1;
+        else if (operators[indexOfCommand] == ";") skip = 0;
+        indexOfCommand++;
+    }
+}
+
+/*
+int main(int argc, char **argv)
+{
+    //char* command[][3] = {{"echo", "a", NULL}, {"echo", "a", NULL}, {"echo", "c", NULL}, {"echo", "d", NULL}};;
+    //char* operators[] = {"||", "&&", ";"};
+
+    char* command[][3] = {{"./b.out", "< in > out", NULL}, {"echo", "a", NULL}, {"echo", "b", NULL} };
+    char* operators[] = {"||",";"};
+
+    executeCommand(command, 3, operators);
+}*/
+
 
 /*
  * User input is read in by initially allocating a buffer
  * size for input and then reallocating more space when needed.
  */
 char *readInput(void) {
-    int bufferSize = BUFFER_SIZE; //Buffer size Macro
-    int position = 0;
-    char *textBuffer = malloc(sizeof (char) * bufferSize);
-    int character;
+    char *userInput = NULL;
+    ssize_t bufferSize = 0; //getline will allocate the buffer
 
-    if(!textBuffer){
-        fprintf(stderr, "Text memory allocation error!\n");
-        exit(EXIT_FAILURE);
-    }
-    while(1){
-
-        character = getchar(); //get next character
-
-        if(character == EOF || character == "\n"){ //EOF or newline signals end of input
-            textBuffer[position] = "\0";
-            return textBuffer;
-        }else{
-            textBuffer[position] = character;
-        }
-        position++;
-
-        if(position >=bufferSize){ //Check if we need to allocate more space
-            bufferSize += BUFFER_SIZE;
-            textBuffer = realloc(textBuffer, bufferSize);
-            if(!textBuffer){
-                fprintf(stderr, "Text memory allocation error!\n");
-                exit(EXIT_FAILURE);
-            }
+    if (getline(&userInput, &bufferSize, stdin) == -1){
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);  // EOF
+        } else  {
+            perror("readline");
+            exit(EXIT_FAILURE);
         }
     }
+
+    return userInput;
 }
 
 #define PARSE_BUFFER 64
@@ -88,31 +161,6 @@ char **parseInput(char *line) {
     return tokens;
 }
 
-int shell_launch(char **pString) {
-    return 0;
-}
-
-int shell_num_builtins() {
-    return 0;
-}
-
-int executeInput(char **args) {
-    int i;
-
-    if (args[0] == NULL) {
-        // An empty command was entered.
-        return 1;
-    }
-
-    for (i = 0; i < shell_num_builtins(); i++) {
-        if (strcmp(args[0], builtin_str[i]) == 0) {
-            return (*builtin_func[i])(args);
-        }
-    }
-
-    return shell_launch(args);
-}
-
 /*
  * Main loop of shell. User input will be read in, split
  * into arguments and then executed.
@@ -126,7 +174,12 @@ void shell_loop() {
         printf("> ");
         userInput = readInput();
         arguments = parseInput(userInput);
-        status = executeInput(arguments);
+        printf("%c", arguments[1][0]);
+
+        struct commandList toExecute = fineParser(arguments);
+
+        executeCommand(toExecute.command, toExecute.numberOfCommands, toExecute.operators);
+
 
         free(userInput);
         free(arguments);
